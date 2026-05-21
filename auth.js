7fcb82b6d80591d2ee015aaf2faaf317f78f27d8
EBAHYTbI ДEД <lakamoikusok2006@gmail.com>
2026-05-21 13:34:04 +0300

    temp link change

--- auth.js
+++ auth.js
@@ -1,5 +1,5 @@
 // auth-api.js
-const API_BASE = 'https://76006142c867419f-176-60-22-202.serveousercontent.com/api/auth';
+const API_BASE = 'https://da6b8a91b96a1785-176-60-22-144.serveousercontent.com/api/auth';
 
 function createHeaders(token = null) {
   const headers = {

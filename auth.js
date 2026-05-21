6518adbc772f71f11240a949e51958f0d2822da4
EBAHYTbI ДEД <lakamoikusok2006@gmail.com>
2026-05-21 14:18:36 +0300

    auth.js API link

--- auth.js
+++ auth.js
@@ -1,5 +1,5 @@
 // auth-api.js
-const API_BASE = 'https://76006142c867419f-176-60-22-202.serveousercontent.com/api/auth';
+const API_BASE = 'https://30fdccdac4cd4d9b-176-60-22-144.serveousercontent.com/api/auth';
 
 function createHeaders(token = null) {
   const headers = {
